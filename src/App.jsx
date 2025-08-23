c560087be7ab6557073e68dd877f7466b22bbf40
giri2205 <girikrishnamurthy123@gmail.com>
2025-08-23 21:28:41 +0530

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,7 @@
 
 import Suggestions from "./Suggestions";
 import Sidebar from "./Sidebar";
-import Feed from "./feed";
+import Feed from "./Feed";
 export default function App() {
   return (
     <div className="app">
